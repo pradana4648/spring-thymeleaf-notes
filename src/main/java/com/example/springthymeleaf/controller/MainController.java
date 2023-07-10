c8194829db65f0f600e0e9680e7ee37b0cc8781f
pradana4648 <wakwaujaim@gmail.com>
2023-07-10 22:10:01 +0700

    added footer

--- src/main/java/com/example/springthymeleaf/controller/MainController.java
+++ src/main/java/com/example/springthymeleaf/controller/MainController.java
@@ -1,7 +1,10 @@
 package com.example.springthymeleaf.controller;
 
+import com.example.springthymeleaf.model.Expertise;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,46 @@ public class MainController {
   public ModelAndView indexPage(ModelMap model) {
     List<String> menus = List.of("about us", "services", "pricing", "blog", "contact");
     model.put("menus", menus);
+    model.put("expertises", expertises());
     return new ModelAndView("index", model);
+  }
+
+  private List<Expertise> expertises() {
+    List<Expertise> results = new ArrayList<>();
+    Expertise expertise;
+
+    // 1
+    expertise = new Expertise();
+    expertise.setName("Expertise");
+    expertise.setDescription(
+        "Our team of experienced experts have the knowledge and expertise to deliver innovative IT solutions that meet your unique needs.");
+    expertise.setImage("images/expertise.svg");
+    results.add(expertise);
+
+    // 2
+    expertise = new Expertise();
+    expertise.setName("Technology");
+    expertise.setDescription(
+        "We stay up to date with the latest trends and technologies in the IT industry, so you can get the most advanced solutions available.");
+    expertise.setImage("images/technology.svg");
+    results.add(expertise);
+
+    // 3
+    expertise = new Expertise();
+    expertise.setName("Solutions");
+    expertise.setDescription(
+        "We take a personalized approach to every project, working closely with you to understand your business and create solutions.");
+    expertise.setImage("images/solution.svg");
+    results.add(expertise);
+
+    // 4
+    expertise = new Expertise();
+    expertise.setName("Results");
+    expertise.setDescription(
+        "Our track record speaks for itself – we've helped businesses of all sizes and industries achieve their goals with our IT solutions.");
+    expertise.setImage("images/result.svg");
+    results.add(expertise);
+
+    return results;
   }
 }
---
+++ src/main/java/com/example/springthymeleaf/model/Expertise.java
@@ -0,0 +1,10 @@
+package com.example.springthymeleaf.model;
+
+import lombok.Data;
+
+@Data
+public class Expertise {
+  private String name;
+  private String image;
+  private String description;
+}
