0956c6a505a238b043163ec50612823b93710f55
pradana4648 <wakwaujaim@gmail.com>
2023-07-15 15:59:17 +0700

    feat: added font-awesome icons support

--- src/main/java/com/example/springthymeleaf/controller/FragmentController.java
+++
@@ -1,9 +0,0 @@
-package com.example.springthymeleaf.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-public class FragmentController {
-}
--- src/main/java/com/example/springthymeleaf/controller/MainController.java
+++ src/main/java/com/example/springthymeleaf/controller/MainController.java
@@ -1,14 +1,14 @@
 package com.example.springthymeleaf.controller;
 
-import com.example.springthymeleaf.model.Expertise;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.springthymeleaf.model.Expertise;
 
 @Controller
 public class MainController {
